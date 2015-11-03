81a6d6e26c86e0040364a6c28aee8126c059368a
Carpediem94 <mat.carpella@gmail.com>
2015-11-04 00:23:02 +0100

    add word.json to randomize the words

--- Hangman/js/app.js
+++ Hangman/js/app.js
@@ -6,63 +6,57 @@ var a = "";
 var win = 0;
 var h = 0;
 var sw = -1;
-
-var hang = ["", "abietto", "atavico", "affrancare", "cane", "gatto", "atterrito"];
+var data;
 
 $(document).ready(function(){       
-  
-  $('#msg-w').hide();
-  $('#msg-l').hide();
-  start();
-  
-  $('.btn-try').click(function() {
-    a = $('#letter').val();
-    control(a);
-  });
-  
-  function start() {
-    $('.hman').append("<img src=\"img/hman-0.png\">");
-    word = Math.round(Math.random() * 5 + 1);
-    secretWord = hang[word];
-    console.log("Secret: " + secretWord);
-    for (var i=0; i<hang[word].length; i++) {
-      maskedWord[i] = "_";
-      $('#secret-word').append(maskedWord[i] + " ");
-    }
-    console.log("Masked: " + maskedWord);
-  }
-  
-  function control (a) {
-    $('#secret-word').empty();
-    sw = secretWord.indexOf(a);
-    console.log("sw: " + sw);
-    //inserisce la nuova lettera nella maskedWord se è presente nella secretWord e incrementa la variabile win
-    for (var i=0; i<hang[word].length; i++) {
-      if(a == secretWord[i]) {
-        $('#secret-word').append(a + " ");
-        maskedWord[i] = a;
-        win++;
-        if (win == hang[word].length) {
-          alert("You won");
-          $('#msg-w').show();
-        }
-      } else {
+   
+  RandomWord(function() {
+
+    $('#msg-w').hide();
+    $('#msg-l').hide();
+    start();
+
+    $('.btn-try').click(function() {
+      a = $('#letter').val();
+      control(a);
+    });
+
+    function start() {
+      $('.hman').append("<img src=\"img/hman-0.png\">");
+      secretWord = word;
+      for (var i=0; i<word.length; i++) {
+        maskedWord[i] = "_";
         $('#secret-word').append(maskedWord[i] + " ");
       }
     }
-    
-    //se la variabile sw è negativa 
-    if(sw < 0) {
-      sw = -1;
-      h++;
-      $('.hman').empty();
-      $('.hman').append("<img src=\"img/hman-" + h + ".png\">");
-      //console.log("h: " + h);
-      if(h == 6) {
-        $('#msg-l').show();
+
+    function control (a) {
+      $('#secret-word').empty();
+      sw = secretWord.indexOf(a);
+      //inserisce la nuova lettera nella maskedWord se è presente nella secretWord e incrementa la variabile win
+      for (var i=0; i<word.length; i++) {
+        if(a == secretWord[i]) {
+          $('#secret-word').append(a + " ");
+          maskedWord[i] = a;
+          win++;
+          if (win == word.length) {
+            $('#msg-w').show();
+          }
+        } else {
+          $('#secret-word').append(maskedWord[i] + " ");
+        }
+      }
+
+      //se la variabile sw è negativa incrementa l'hangman 
+      if(sw < 0) {
+        sw = -1;
+        h++;
+        $('.hman').empty();
+        $('.hman').append("<img src=\"img/hman-" + h + ".png\">");
+        if(h == 6) {
+          $('#msg-l').show();
+        }
       }
     }
-  }
-  
-  
+  });
 });
