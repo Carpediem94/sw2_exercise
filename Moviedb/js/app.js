6c9b9ef28a980327b6817190f5b1a91035e89cd6
Carpediem94 <mat.carpella@gmail.com>
2015-11-04 16:20:02 +0100

    bug fix: posso ricercare più volte la stessa parola

--- Moviedb/js/app.js
+++ Moviedb/js/app.js
@@ -1,4 +1,6 @@
 var url = "http://www.omdbapi.com/?s=";
+var ver = true;
+var film = [];
 
 $(document).ready(function () {
   
@@ -12,19 +14,29 @@ $(document).ready(function () {
       console.log("we process the output");
 
       for(var i=0; i<json.Search.length; i++) {
-        $('.movies').append(
-                          '<div class="col-sm-6 col-md-4">' +
-                          ' <div class="thumbnail">' +
-                          '   <div class="text-center">' +
-                          '     <img src="' + json.Search[i].Poster + '" alt="poster">' +                
-                          '       <div class="caption">' +
-                          '         <h3 class="truncate">' + json.Search[i].Title + '</h3>' +                          
-                          '         <a href="http://www.imdb.com/title/' + json.Search[i].imdbID + '" class="btn btn-primary" role="button">Details</a>' +
-                          '       </div' +
-                          '   </div>' +
-                          ' </div>' +
-                          '</div>');
-                          }
+        for(var j=0; j<film.length; j++) {
+          if(json.Search[i].Title == film[j]) {
+            ver = false;  
+          }
+        }
+        if (ver) {
+          $('.movies').append(
+                            '<div class="col-sm-6 col-md-4">' +
+                            ' <div class="thumbnail">' +
+                            '   <div class="text-center">' +
+                            '     <img src="' + json.Search[i].Poster + '" alt="poster">' +                
+                            '       <div class="caption">' +
+                            '         <h3 class="truncate">' + json.Search[i].Title + '</h3>' +                          
+                            '         <a href="http://www.imdb.com/title/' + json.Search[i].imdbID + '" class="btn btn-primary" role="button">Details</a>' +
+                            '       </div' +
+                            '   </div>' +
+                            ' </div>' +
+                            '</div>');
+          }
+        film[i] = json.Search[i].Title;
+        ver = true;
+      }
+      film = [];
     })
     .done(function() {
       console.log( "second success" );
