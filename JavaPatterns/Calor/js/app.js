d32f4e88987ba5d2f018ed424150d1b3b3f493b4
Carpediem94 <mat.carpella@gmail.com>
2015-12-10 17:18:09 +0100

    first version

---
+++ JavaPatterns/Calor/js/app.js
@@ -0,0 +1,84 @@
+/* your code should go here */
+
+$(document).ready(function(){
+
+  var model = {
+    pos: 0,
+    
+    minTemp: function(day) {
+      var pos = this.pos;
+      var tmp = data[pos].temperature;
+      var currentDay = day;
+      for(var i=pos; i<(pos+4); i++) {
+        if(((data[i].temperature < tmp)) && (data[i].day==currentDay)) {
+          tmp = data[i].temperature;
+        }
+      }
+      return tmp;
+    },
+    
+    maxTemp: function(day) {
+      var pos = this.pos;
+      var tmp = data[pos].temperature;
+      var currentDay = day;
+      for(var i=pos; i<(pos+4); i++) {
+        if(((data[i].temperature > tmp)) && (data[i].day==currentDay)) {
+          tmp = data[i].temperature;
+        }
+      }
+      return tmp;
+    }
+  };
+  
+  var octopus = {
+    init: function() {
+      viewMeteo.init();
+    },
+    
+    incrementPos: function() {
+      model.pos += 4;
+    },
+    
+    getPos: function() {
+      return model.pos;
+    },
+    
+    getTempMin: function(day) {
+      return model.minTemp(day);
+    },
+    
+    getTempMax: function(day) {
+      return model.maxTemp(day);
+    }
+    
+  };
+  
+  var viewMeteo = {
+    init: function() {
+      this.meteo = $('#summary');
+      this.render();
+    },
+    
+    render: function() {
+      var pos;
+      for(var i=0; i<4; i++) {
+        pos = octopus.getPos();
+        day = data[pos].day;
+        this.meteo.append('<li>' +
+                        '<div class="icon">' +
+                          '<img src="img/icons/'+ data[pos].condition + '.png">' +      
+                        '</div>' +
+                        '<div class="stats">' +  
+                          '<h2>' + day + '</h2>' +
+                          '<strong>min</strong> ' + octopus.getTempMin(day) + ' ' +
+                          '<strong>max</strong> ' + octopus.getTempMax(day) +   
+                        '</div>' + 
+                      '</li>');
+        octopus.incrementPos();
+      }
+      
+    }
+  };
+  
+  octopus.init();
+});
