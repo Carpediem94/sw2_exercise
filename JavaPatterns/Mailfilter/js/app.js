837ec1470c62faad90ff3d27d5bdad7264298817
Carpediem94 <mat.carpella@gmail.com>
2015-12-12 18:37:29 +0100

    first version

---
+++ JavaPatterns/Mailfilter/js/app.js
@@ -0,0 +1,87 @@
+/* your code should go here */
+
+
+// You can modify this object, add functions that you need
+var MailModel = {
+  /**
+   * Initialises the model with the "database" of filter rules
+   * and messages. This function is already implemented.
+   */
+   init : function(){
+     this.rules = rules;
+     this.messages = msgs;
+   }, 
+  
+   /**
+    * Filters out messages in the "database" that match the spam rules.
+    * @return an array of messages, excluding those that match the filter rules.
+    */
+    filter : function() {
+      var filter = [];
+      var control = true;
+      for(var i=0; i<this.messages.length; i++) {
+        for(var j=0; j<this.rules.length; j++) {
+          if(this.messages[i].indexOf(this.rules[j]) !== -1) {
+            control = false;
+          }  
+        }
+        
+        if(control) {
+          filter.push(this.messages[i]);
+        }
+        control = true;
+      }
+      
+      return filter;
+    } 
+};
+
+// Example of usage:
+// MailModel.init()
+// MailModel.filter() 
+//  -> ["carlo@gmail.com", "trentose2@googlegroups.com"]
+var octopus = {
+  init: function() {
+    MailModel.init();
+    viewMail.init();
+    viewMail.runFilter();
+  },
+  
+  getMsgs: function() {
+    return MailModel.messages;
+  },
+  
+  filterMsgs: function() {
+    var msgs = MailModel.filter();
+    MailModel.messages = msgs;
+  }
+};
+
+// We suggest to use js patters. 
+// you can add here your views and controllers if you decide to do so.
+var viewMail = {
+  init: function() {
+    this.mailList = $('.result');
+    this.render();
+  },
+  
+  render: function() {
+    this.mailList.empty();
+    var msgs = octopus.getMsgs();
+    for(var i=0; i<msgs.length; i++) {
+      this.mailList.append('<li>' + msgs[i] + '</li>');
+    }
+  },
+  
+  runFilter: function() {
+    $('.btn-filter').click(function() {
+      octopus.filterMsgs();
+      viewMail.render();
+    });
+  }
+}
+
+
+$(document).ready(function(){
+  octopus.init();
+});
